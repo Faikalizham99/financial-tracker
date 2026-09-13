6c37daa174b3f997174d166c23dd27c4fe894483
faikalizham <faikalizham@gmail.com>
2026-09-13 19:37:32 +0800

    Initial FinancialTracker MAUI project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FinancialTracker;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FinancialTracker;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
